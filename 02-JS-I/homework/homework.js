39b9085f3b0c2b8f102c7930fb3b5e1def7c481c
edirora1989 <95984936+edirora1989@users.noreply.github.com>
2021-12-26 20:15:56 -0500

    miprimercommit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = 'ole';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '2';
+const nuevoNum = 2;
 
 // Crea una variable booleana:
-const nuevoBool = 'true' ;
+const nuevoBool = true ;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -31,12 +31,12 @@ function devolverString(str) {
               return str;
 }
 
-function suma(x, y) {
+function sumar(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-              var suma = x+y;
-              return suma;
+              var sumar = x+y;
+              return sumar;
 }
 
 function resta(x, y) {
@@ -74,7 +74,13 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-      if
+      if (str1==str2){
+                      return true
+      }
+          else{
+               return false
+                    }
+  
   
 }
 
@@ -92,7 +98,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-         if(nun>50)  {
+         if(num>50)  {
            return true;
                } 
                return false;  
@@ -101,6 +107,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+    if (x%y)
   
 }
 
@@ -238,7 +245,7 @@ module.exports = {
   sonIguales,
   menosQueNoventa,
   mayorQueCincuenta,
-  suma,
+  sumar,
   resta,
   divide,
   multiplica,
