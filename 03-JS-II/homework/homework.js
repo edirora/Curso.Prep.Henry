3c62e1a186bc665b37b4f7d43caeb09b4c3676cd
edirora <edison.rodriguez@correounivalle.edu.co>
2022-07-02 21:01:06 -0500

    homework.final

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -34,15 +34,6 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-
-        if (status===1){
-          return "Online";
-        } else if(status===2){
-                  return  "Away";
-        }
-        else{
-              return "Offline";
-           }
 }
 
 function saludo(idioma) {
@@ -52,18 +43,6 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-      if(idioma === "aleman"){
-          return "Guten Tag!";
-          }
-          if(idioma === "ingles"){
-                return "Hello!" ;
-          }
-            else if(idioma === "mandarin"){
-                  return "Ni Hao!";
-            }
-              else{ 
-                return "Hola!";
-              } 
 }
 
 function colors(color) {
@@ -74,31 +53,18 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
- // var recibe;
-  switch (color){
-  case "blue" :
-      return `This is blue`;
-  case "red" :
-      return "This is red";
-  case `green` :
-      return "This is green";
-  case "orange" :
-      return "This is orange";
-   default :
-      return  "Color not found"
-      }
 }
-   
+
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero === 10 || numero === 5){
-         return true;
-   }
-           else{
-          return false;
-   }
+  if (numero = 10 || 5){
+                    return true;
+                      }
+                      else{
+                        return false;
+                      }
 }
 
 function estaEnRango(numero) {
@@ -123,12 +89,6 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(Math.floor(numero) || numero % 1===0){
-        return true;
-  }
-      else{
-        return false;
-      }
 }
 
 function fizzBuzz(numero) {
@@ -136,18 +96,6 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 15 === 0){
-    return "fizzbuzz";
-  }
-  else if (numero % 3 === 0){
-      return "fizz";
-  }
-      else if(numero % 5 === 0){
-        return "buzz";
-      }
-       else{
-            return numero;
-          }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -156,27 +104,7 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-  if (num1<0 || num2<0 || num3<0) {
-    return "Hay negativos" ;
-}
-    else 
-      if (num1 === 0 || num2 === 0 || num3 ===0){
-         return "Error";
-}
-       else
-       if ((num3 > num1) && (num3 > num2)){
-          num3 = num3 +1;
-          return num3;
-        } 
-        else
-        if ((num1 != (num2 && num3)) && ((num1 > num2 && num3) && (num1 > 0))) {
-        return "Número 1 es mayor y positivo";
-        }        
-          else
-          if(num1<num2 || num1>num3 || num3>(num1 && num2)){
-            return false;
-          }
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 }
 
 function esPrimo(numero) {
@@ -185,78 +113,32 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- 
-  var esPrim = true;
- 
-  if(numero === 0 || numero ===1){
-      esPrim = false;
-  }
-  else
-  {
-      for (var i=2; i < numero; i++)
-      {
-        if(numero % i === 0)
-        {
-          esPrim = false;
-        }
-      }
-  }
-  return esPrim;
-
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor == true){
-      return "Soy verdadero";
-     }
-      if (valor == false){
-        return "Soy falso"
-      }
+
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí
-  var tabla =  [0,1,2,3,4,5,6,7,8,9,10];
-  var seis = 6;
-  var total = tabla;
-
-  for (var i=0; i < tabla.length; i++)
-  {
-     total[i] = (tabla[i]*seis);
-   }
-     return total;
+  //Escribe tu código aquí   
+  
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero.toString().length===3)
-  {
-    return true;
-  }
-  else {
-    return false;
-  }
+  
 }
 
-function doWhile(numero=0) {
+function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  //let numero = 0;
-  let i = 0;
-
-do {
-  i = i + 1;
-  numero = numero + 5;
-} while (i < 8);
-  return  numero;
-  
 }
 
 
--- 08-HTML/homework/README.md
+++ 08-HTML/homework/README.md
@@ -25,17 +25,17 @@ I. Cree una página HTML básica.
 ```
 II Añade un poco de estilo
   A. Agregue la etiqueta de estilo a su documento HTML.
-  B. Asignar al tercer div el id "thirdDiv".
-  C. Asignar a los otros divs la clase "divClass".
-  D. Asignar al span que se encuentra dentro de su segundo div el id "spanId".
-  E. Agregue una regla de estilo a su elemento h1 cambiando el color del texto (Listado de colores: https://www.crockford.com/wrrrld/color.html).
-  F. Agregue una regla de estilo al elemento img dándole un ancho de 400px.
-  G. Agregue una regla de estilo a 'thirdDiv' cambiando la altura a 600px y el ancho a 500px.
-  H. Agregue una regla de estilo a 'thirdDiv' cambiando el color de fondo por el que quiera.
-  I. Cambie el tamaño de la fuente para 'spanId' a 18px.
-  J. Asignar al id 'spanId' un margen de 50px.
-  K. Asignar al id 'thirdDiv' un relleno (padding) de 50px.
-  L. Asignar al id 'thirdDiv' un borde con cualquier ancho de color y estilo que elija.
+  B. Asignar al tercer div el id "thirdDiv". ok
+  C. Asignar a los otros divs la clase "divClass". ok
+  D. Asignar al span que se encuentra dentro de su segundo div el id "spanId". 0k
+  E. Agregue una regla de estilo a su elemento h1 cambiando el color del texto (Listado de colores: https://www.crockford.com/wrrrld/color.html)0k.
+  F. Agregue una regla de estilo al elemento img dándole un ancho de 400px. 0k
+  G. Agregue una regla de estilo a 'thirdDiv' cambiando la altura a 600px y el ancho a 500px.0k
+  H. Agregue una regla de estilo a 'thirdDiv' cambiando el color de fondo por el que quiera.0k
+  I. Cambie el tamaño de la fuente para 'spanId' a 18px.0k
+  J. Asignar al id 'spanId' un margen de 50px.0k
+  K. Asignar al id 'thirdDiv' un relleno (padding) de 50px. 0k
+  L. Asignar al id 'thirdDiv' un borde con cualquier ancho de color y estilo que elija.0k
 ```
 
 ```
